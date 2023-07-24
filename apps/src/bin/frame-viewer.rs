ac6ec51b5e27b472fc5b48392f366d13f918b199
chernov <kapot65@gmail.com>
2023-07-24 15:10:10 +0300

    fix frame-viewer count rate calculation

--- apps/src/bin/frame-viewer.rs
+++ apps/src/bin/frame-viewer.rs
@@ -1,11 +1,12 @@
 use std::collections::BTreeMap;
-use std::net::{SocketAddr, UdpSocket};
-use std::sync::{Arc, Mutex};
+use std::net::SocketAddr;
+use std::sync::Arc;
 use std::time::Instant;
 
 use clap::Parser;
 use eframe::egui;
 use eframe::egui::plot::{Legend, Plot};
+use egui::mutex::Mutex;
 
 use apps::defaults::BOARD_IP;
 use processing::{process_waveform, waveform_to_events, Algorithm, ProcessedWaveform, EguiLine, color_for_index};
@@ -20,7 +21,7 @@ struct Args {
     #[arg(long, default_value_t = BOARD_IP)]
     tqdc_ip: std::net::IpAddr,
 
-    #[arg(long, default_value_t = 0.0)]
+    #[arg(long, default_value_t = 25.0)]
     hist_min: f32,
 
     #[arg(long, default_value_t = 400.0)]
@@ -28,13 +29,9 @@ struct Args {
 
     #[arg(long, default_value_t = 400)]
     hist_bins: usize,
-
-    /// count rate accumulating interval in seconds
-    #[arg(long, default_value_t = 5.0)]
-    count_rate_interval: f32,
     
     /// graphics refresh rate in Hz
-    #[arg(long, default_value_t = 10.0)]
+    #[arg(long, default_value_t = 1.0)]
     refresh_rate: f32,
 
     #[arg(long, default_value_t = 25.0)]
@@ -44,12 +41,11 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let empty_hist = PointHistogram::new(
+    let histogram_bg = Arc::new(Mutex::new(PointHistogram::new(
         args.hist_min..args.hist_max,
         args.hist_bins,
-    );
+    )));
 
-    let histogram_bg = Arc::new(Mutex::new(empty_hist.clone()));
     let histogram = Arc::clone(&histogram_bg);
 
     let channels_bg = Arc::new(Mutex::new(BTreeMap::new()));
@@ -61,7 +57,7 @@ fn main() {
     std::thread::spawn(move || {
         let tqdc_address = SocketAddr::new(args.tqdc_ip, TQDC_STREAM_PORT);
 
-        let sock = UdpSocket::bind("0.0.0.0:0").unwrap();
+        let sock = std::net::UdpSocket::bind("0.0.0.0:0").unwrap();
         
         sock.send_to(
             &MlinkMessage::to_datagram(&MlinkMessage::new_stream_acq(
@@ -72,17 +68,13 @@ fn main() {
                 0xFFFF,
             )),
             tqdc_address,
-        )
-        .unwrap();
+        ).unwrap();
 
-        let mut count_rate_timer = Instant::now();
-        let mut plots_refresh_timer = Instant::now();
+        let mut refresh_timer = Instant::now();
         
-        let mut counts = BTreeMap::new();
-        let mut channels = BTreeMap::new();
-        let mut histogram = empty_hist.clone();
-
-        let mut fragments_buf: BTreeMap<u16, Vec<tqdc::mstream::MStreamFragment>> = BTreeMap::new();
+        let counts = Arc::new(Mutex::new(BTreeMap::new()));
+        
+        let mut fragments_buf: BTreeMap<u16, BTreeMap<u16, tqdc::mstream::MStreamFragment>> = BTreeMap::new();
         
         let mut seq = 0;
 
@@ -91,7 +83,6 @@ fn main() {
             let (len, to_addr) = sock.recv_from(&mut buf).unwrap();
             let message = MlinkMessage::from_datagram(&buf[..len]);
 
-            let count_rate_interval_ms = (args.count_rate_interval * 1000.0) as u128;
             let plots_refresh_interval_ms = ( 1.0 / args.refresh_rate * 1000.0) as u128;
 
             match message {
@@ -105,45 +96,61 @@ fn main() {
                             fragment.header.fragment_id,
                         )),
                         to_addr,
-                    )
-                    .unwrap();
+                    ).unwrap();
                     seq +=1;
 
-                    fragments_buf.entry(fragment.header.fragment_id).or_insert(vec![]).push(fragment);
+                    fragments_buf.entry(fragment.header.fragment_id).or_insert(BTreeMap::new()).insert(
+                        fragment.header.fragment_offset, fragment
+                    );
 
-                    let elapsed_ms = count_rate_timer.elapsed().as_millis();
-                    if elapsed_ms > count_rate_interval_ms {
-                        count_rate_timer = Instant::now();
-                        counts.iter_mut().for_each(|(_, count)| {
-                            *count /= (elapsed_ms) as f32 / 1000.0;
-                        });
-                        *count_rate_bg.lock().unwrap() = counts.clone(); // TODO: fix count rate calculation
-                        counts.clear();
-                    }
-
-                    let elapsed_ms = plots_refresh_timer.elapsed().as_millis();
+                    let elapsed_ms = refresh_timer.elapsed().as_millis();
                     if elapsed_ms > plots_refresh_interval_ms {
-                        fragments_buf.iter().for_each(|(_, fragments)| {
-                            let fragments = fragments.iter().collect::<Vec<_>>();
-                            if let Ok(combined) = MStreamTriggerAndUserData::try_from(&fragments[..]) {
-                                channels = combined.extract_data_blocks().into_iter().map(|ADCDataBlock {ch_num, waveform }| {
-                                    let waveform = process_waveform(waveform);
-                                    waveform_to_events(&waveform, &Algorithm::Max).into_iter().for_each(|(_, amp)| {
-                                        if amp > args.count_rate_threshold {
-                                            *counts.entry(ch_num).or_insert(0.0) += 1.0;
-                                        }
-                                        histogram.add(ch_num, amp)
-                                    });
-                                    (ch_num, waveform)
-                                }).collect::<BTreeMap<_, _>>();
-                            }
-                        });
-                        fragments_buf.clear();
 
-                        plots_refresh_timer = Instant::now();
-                        *channels_bg.lock().unwrap() = channels.clone();
-                        channels.clear();
-                        *histogram_bg.lock().unwrap() = histogram.clone();
+                        let channels_bg = Arc::clone(&channels_bg);
+                        let histogram_bg = Arc::clone(&histogram_bg);
+                        let counts = Arc::clone(&counts);
+
+                        let count_rate_bg = Arc::clone(&count_rate_bg);
+
+                        fragments_buf = {
+
+                            std::thread::spawn(move || {
+                                let mut counts = counts.lock();
+                                let mut histogram = histogram_bg.lock();
+    
+                                fragments_buf.iter().for_each(|(_, fragments)| {
+                                    let fragments = fragments.values().collect::<Vec<_>>();
+                                    if let Ok(combined) = MStreamTriggerAndUserData::try_from(&fragments[..]) {
+                                        *channels_bg.lock() = combined.extract_data_blocks().into_iter().map(|ADCDataBlock {ch_num, waveform }| {
+                                            let waveform = process_waveform(waveform);
+                                            waveform_to_events(&waveform, &Algorithm::Max).into_iter().for_each(|(_, amp)| {
+                                                if amp > args.count_rate_threshold {
+                                                    *counts.entry(ch_num).or_insert(0.0f64) += 1.0;
+                                                }
+                                                histogram.add(ch_num, amp)
+                                            });
+                                            (ch_num, waveform)
+                                        }).collect::<BTreeMap<_, _>>();
+                                    } else {
+                                        println!("failed to combine fragments");
+                                        fragments.iter().for_each(|fr| println!("{:?}", fr.header));
+                                        println!("=====");
+                                    }
+                                });
+
+                                counts.iter_mut().for_each(|(_, count)| {
+                                    *count /= (elapsed_ms) as f64 / 1000.0;
+                                });
+                                
+                                *count_rate_bg.lock() = counts.clone();
+                                counts.clear();
+
+                                fragments_buf.clear();
+                            });
+
+                            refresh_timer = Instant::now();
+                            BTreeMap::new()
+                        };
                     }
                 }
                 _ => panic!("unimplemented!!"),
@@ -153,7 +160,7 @@ fn main() {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "My egui App",
+        "frame-viewer",
         native_options,
         Box::new(|_| {
             Box::new(MyEguiApp {
@@ -168,17 +175,17 @@ fn main() {
 struct MyEguiApp {
     histogram: Arc<Mutex<PointHistogram>>,
     channels: Arc<Mutex<BTreeMap<u8, ProcessedWaveform>>>,
-    count_rate: Arc<Mutex<BTreeMap<u8, f32>>>,
+    count_rate: Arc<Mutex<BTreeMap<u8, f64>>>,
 }
 
 impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
         ctx.request_repaint_after(std::time::Duration::from_millis(1000 / 60));
 
-        let channels = self.channels.lock().unwrap().clone();
+        let channels = self.channels.lock().clone();
 
         egui::TopBottomPanel::top("count_rates").show(ctx, |ui| {
-            let count_rate_lock = self.count_rate.lock().unwrap();
+            let count_rate_lock = self.count_rate.lock();
 
             ui.label("count_rates:");
             for (ch_num, count_rate) in count_rate_lock.iter() {
@@ -216,7 +223,7 @@ impl eframe::App for MyEguiApp {
                             position: egui::plot::Corner::RightTop,
                         })
                         .show(ui, |plot_ui| {
-                            let hist = self.histogram.lock().unwrap().clone();
+                            let hist = self.histogram.lock().clone();
                             hist.draw_egui_each_channel(plot_ui, None);
                         });
                 });
